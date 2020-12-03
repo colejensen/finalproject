a37ff082dd0a6aaf06b8f9ae75a50deb6c3e705b
Cole Jensen <61286478+colejensen@users.noreply.github.com>
2020-12-03 18:25:00 -0500

    Include the "All In One" Figure code

--- scripts/boxplots.r
+++ scripts/boxplots.r
@@ -15,3 +15,18 @@ boxplot(data$Oceania, main="Introductions From NYC to Oceania", xlab="Ocenia", c
 boxplot(data$ME, main="Introductions From NYC to Middle East", ylab="Number of Introductions", xlab="Middle East", col="white", horizontal = TRUE)
 
 par(mfrow=c(1,1))
+
+
+# make a single boxplot with all the information 
+# No facet
+library(ggplot2)
+ggplot(data=data, aes(x='Where Introductions Occured', y='Number of Introdiuctions')) + geom_boxplot()
+
+
+data <- data.frame("Total" = c(79,79,97), "EU" = c(38,38,48), "Asia" = c(2,2,4), "NCA" = c(12,12,11), "Oceania" = c(4,4,4), 
+                   "SA" = c(14,14,18), "Africa"=c(2,2,5), "ME"=c(7,7,7))
+test <- data.frame("Values"= c(79,79,97,38,38,48,2,2,4,12,12,11,4,4,4,14,14,18,2,2,5,7,7,7), "Location" = 
+                     c("Total","Total","Total","Europe","Europe","Europe","Asia","Asia","Asia","N.C.A.", "N.C.A.","N.C.A.",
+                       "Oceania","Oceania","Oceania","S.A.","S.A.","S.A.","Africa","Africa","Africa","Middle East","Middle East","Middle East"))
+
+boxplot(Values~Location, data=test, main="Introductions from New York", xlab="Location", ylab="Number of Introductions", col="steelblue", border="black")
